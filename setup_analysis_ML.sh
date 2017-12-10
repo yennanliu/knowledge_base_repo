d35d7642152ed8fbd5b0dd3207341cd96eda006d
yennanliue <f339339@gmail.com>
2017-12-10 22:16:00 +0000

    add comments

--- setup_analysis_ML.sh
+++ setup_analysis_ML.sh
@@ -4,8 +4,14 @@
 # Shell that help build up python based analysis & ML env 
 # python 3 
 
+# ref : https://gist.github.com/mlgill/4302c24ad1c8999577fd2f6cd03d8d2b
+
 echo 'install anaconda...'
 echo 'plz download via  :  https://www.anaconda.com/download/#macos'
+
+echo 'set dev env...'
+# conda create -n datasci
+# source activate datasci
 
 echo 'plz make sure you are using python 3 '
 echo 'your current python verison : ' # && python -c 'import sys; print (sys.version) '
@@ -18,7 +24,10 @@ echo '--------------------------------'
 echo 'install needed packages....'
 echo ''
 echo '--------------------------------'
-pip install pandas numpy sklearn scipy matplotlib SQLAlchemy seaborn
+pip install pandas numpy sklearn scipy matplotlib SQLAlchemy seaborn jupyter statsmodels 
 
 
-echo 'config DB'
+echo 'config python variable'
+
+export PYTHONPATH=$PYTHONPATH:/anaconda/envs/<your_dev_env_name>/lib/python3.4
+
