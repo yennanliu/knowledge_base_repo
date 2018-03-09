a589e84997f228468fefab5df070715f35324391
yennanliue <f339339@gmail.com>
2018-03-09 11:04:50 +0000

    add commit and push docker commands

--- docker_ops.sh
+++ docker_ops.sh
@@ -5,15 +5,36 @@
 # https://towardsdatascience.com/how-docker-can-help-you-become-a-more-effective-data-scientist-7fc048ef91d5
 
 
-
-
+#-------------------------------------------------------------------------------------------------------
+# build docker from Dockerfile -> run docker  -> commit status of docker  -> push docker image to cloud # 
+#-------------------------------------------------------------------------------------------------------
 
 
 # -- build docker from Dockerfile
 #sudo docker build -t mac_ds_env -f ./Dockerfile ./
-sudo docker build -t 'docker_name' -f 'docker_directory' ./
+sudo docker build -t <docker_name> -f <docker_directory> ./
 
 
 
 # -- run docker 
-docker run -it --name mac_ds_env --net=host -v ~/docker_file/:/ds mac_ds_env
+#docker run -it --name mac_ds_env --net=host -v ~/docker_file/:/ds mac_ds_env
+docker run -it --name <docker_name> --net=host -v ~/docker_file/:/<directory_inside_docker> <image_name>
+
+
+
+# -- commit status of docker 
+#docker commit mac_ds_env yennanliu/mac_ds_env:v1
+docker commit <comtainer_name> yennanliu/<docker_name>:<version>
+
+
+# -- push docker image to cloud 
+#docker push yennanliu/mac_ds_env:v1
+docker push <docker_account>/<docker_name>:<version>
+
+
+
+
+
+
+
+
