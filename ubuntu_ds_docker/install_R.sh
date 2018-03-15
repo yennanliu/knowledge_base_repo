b7ccf3b5107b48a912a9f68edf2afc2186202c7b
yennanliu <f339339@gmail.com>
2018-03-15 16:14:31 +0000

    add install R code

---
+++ ubuntu_ds_docker/install_R.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+
+
+#Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install ruby
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+# install R 
+brew tap homebrew/science
+brew install r
