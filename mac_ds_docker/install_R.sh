331ce5d06c9b9a29d183c3ac6f4d9fae02395fa8
yennanliu <f339339@gmail.com>
2018-03-15 15:03:36 +0000

    add R installation to dockerfile

---
+++ mac_ds_docker/install_R.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+
+
+#Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+# install R 
+brew tap homebrew/science
+brew install r
