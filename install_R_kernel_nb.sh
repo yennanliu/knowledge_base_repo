f77401e03da18bb40721cf86a80b03ac699fd820
yennanliue <f339339@gmail.com>
2018-07-14 10:14:33 +0100

    add ref/comments

--- install_R_kernel_nb.sh
+++ install_R_kernel_nb.sh
@@ -19,6 +19,8 @@ conda install -c r ipython-notebook r-irkernel
 R  
 # ------------ below commands is under R env (R command line) ------------ #
 # step 4) intall other needed libraries under R command line 
+# https://www.r-bloggers.com/installing-r-packages/
+# installing to /Users/yennanliu/anaconda3/envs/python_R/lib/R/library/utf8/libs
 install.packages(c('rzmq','repr','IRkernel','IRdisplay'), repos = 'http://irkernel.github.io/', type = 'source')
 # step 5) After the packages are successfully downloaded and installed. Type this:
 IRkernel::installspec()
