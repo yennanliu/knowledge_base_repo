69eb3a467b1f7714c2d05f788673adafc9a953dd
yennanliu <f339339@gmail.com>
2018-10-03 17:12:50 +0100

    add needed install script

---
+++ mac_ds_docker/jupyter_notebook_config.py
@@ -0,0 +1,13 @@
+c = get_config()  # get the config object
+c.IPKernelApp.pylab = 'inline'  # in-line figure when using Matplotlib
+c.NotebookApp.ip = '*'  
+c.NotebookApp.open_browser = False  # do not open a browser window by default when using notebooks
+c.NotebookApp.token = '' # No token. Always use jupyter over ssh tunnel
+c.NotebookApp.notebook_dir = '/notebooks'
+c.NotebookApp.allow_root = True # Allow to run Jupyter from root user inside Docker container
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.tornado_settings = { 
+    'headers': { 
+        'Content-Security-Policy': "frame-ancestors 'self' *" 
+    }
+}
