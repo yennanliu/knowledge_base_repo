6cf726ad98ba691eb87f2efe73d6583229fe1559
yennanliue <f339339@gmail.com>
2019-04-16 09:36:30 +0100

    fix docker build/commit/push commands and run them via shell scripts

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#################################################################
+# SHELL SCRIPT BUILD DOCKER INSTANCE FOR TRAVIS CI   
+#################################################################
+
+echo ' ---------------- BUILD ALL REPO DOCKER IMAGES ----------------'
+
+declare -a docker_images=("mac_de_docker/."  "de_airflow_docker_dev/.")
+
+for docker_image in "${docker_images[@]}"
+	do 
+		instance_name="$(cut -d'/' -f1 <<<"$docker_images")"
+		# docker build 
+		echo 'docker bulid : ' $docker_image  && docker build $docker_image -t $instance_name	
+		# run test 
+		docker run -it $instance_name  echo 'docker test 123'	
+	done 
---
+++ deploy_dockerhub.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#################################################################
+# SHELL SCRIPT DEPLOY DOCKER IMAGE TO DOCKER HUB 
+#################################################################
+
+echo ' ---------------- DEPLOY TO DOCKER HUB ----------------'
+
+declare -a docker_images=("mac_de_docker/."  "de_airflow_docker_dev/.")
+#REGISTRY_USER=$(echo ${REGISTRY_USER})
+#REGISTRY_PASS=$(echo ${REGISTRY_PASS})
+echo "$REGISTRY_PASS" | docker login  --username $REGISTRY_USER --password $REGISTRY_PASS && echo "docker login OK" || echo "docker login failed"
+
+for docker_image in "${docker_images[@]}"
+	do 
+		instance_name="$(cut -d'/' -f1 <<<"$docker_image")"
+		container_id="` docker ps -a | awk 'FNR == 2 {print $1}'`" && echo container_id = $container_id && image_id="` docker ps -a | awk 'FNR == 2 {print $2}'`" && echo image_id = $image_id 
+		# docker deploy 
+		echo 'COMMIT & DEPLOY  : ' $docker_image  && docker commit $container_id yennanliu/$instance_name:V1 && docker push yennanliu/$instance_name:V1
+	done 
