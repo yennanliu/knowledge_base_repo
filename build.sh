d4f094ff585c24858ff0e8d64dfe22c94fabdd6a
yennanliu <f339339@gmail.com>
2019-09-04 16:37:43 +0800

    update to-build docker image

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@
 echo ' ---------------- BUILD ALL REPO DOCKER IMAGES ----------------'
 
 echo "$REGISTRY_PASS" | docker login  --username $REGISTRY_USER --password $REGISTRY_PASS && echo "docker login OK" || echo "docker login failed"
-declare -a docker_images=("mac_ds_ml_docker/.")
+declare -a docker_images=("pyspark_notebook_docker/.")
 for docker_image in "${docker_images[@]}"
 	do 
 		instance_name="$(cut -d'/' -f1 <<<"$docker_images")"
 		# docker build 
 		echo 'docker bulid : ' $docker_image  && docker build $docker_image -t $instance_name	
 		# run test 
-		docker run -it $instance_name  echo 'docker test 123'	
+		#docker run -it $instance_name  echo 'docker test 123'	
 	done 
