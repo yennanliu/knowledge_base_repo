8cff5a40e92bd921910cfe1271eb237019ac18a4
yennanliu <f339339@gmail.com>
2019-09-11 11:14:51 +0800

    fix code layout

--- deploy_dockerhub.sh
+++ deploy_dockerhub.sh
@@ -12,9 +12,9 @@ declare -a docker_images=("spark_stand_alone/.")
 echo "$REGISTRY_PASS" | docker login  --username $REGISTRY_USER --password $REGISTRY_PASS && echo "docker login OK" || echo "docker login failed"
 
 for docker_image in "${docker_images[@]}"
-	do 
-		instance_name="$(cut -d'/' -f1 <<<"$docker_image")"
-		container_id="` docker ps -a | awk 'FNR == 2 {print $1}'`" && echo container_id = $container_id && image_id="` docker ps -a | awk 'FNR == 2 {print $2}'`" && echo image_id = $image_id 
-		# docker deploy 
-		echo 'COMMIT & DEPLOY  : ' $docker_image  && docker commit $container_id yennanliu/$instance_name:V1 && docker push yennanliu/$instance_name:V1
-	done 
+  do 
+    instance_name="$(cut -d'/' -f1 <<<"$docker_image")"
+    container_id="` docker ps -a | awk 'FNR == 2 {print $1}'`" && echo container_id = $container_id && image_id="` docker ps -a | awk 'FNR == 2 {print $2}'`" && echo image_id = $image_id 
+    # docker deploy 
+    echo 'COMMIT & DEPLOY  : ' $docker_image  && docker commit $container_id yennanliu/$instance_name:V1 && docker push yennanliu/$instance_name:V1
+  done 
